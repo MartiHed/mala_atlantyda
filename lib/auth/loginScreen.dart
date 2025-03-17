a0565cce5df383e6229944e86c79b5331c8b0dd0
MartiHed <martyna.hedeszynska@gmail.com>
2025-03-18 00:15:08 +0100

    pozmienialam, powalczylam i srednio idzie wiec moze tobie sie uda

---
+++ lib/auth/loginScreen.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../widgets/customTextField.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> login(String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: "grupa1@gmail.com", // Tutaj wpisujesz stały email
+        password: password, // Hasło wprowadzone przez użytkownika
+      );
+      print("✅ Zalogowano: ${userCredential.user!.email}");
+    } catch (e) {
+      print("❌ Błąd logowania: $e");
+    }
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xFF0c4767),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+            Image.asset('images/logoM_A.png', fit: BoxFit.cover, height: 300,),
+            Text(
+              "Wpisz hasło grupy",
+              style: GoogleFonts.kanit(
+                textStyle: const TextStyle(
+                  color: Color(0xFF0c4767),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            CustomTextField(
+                hint: "Enter Password",
+                label: "Password",
+                //controller: _password,
+                isPassword: true,
+                color: Color(0xFFADE8F4)
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: (Text("Tekst")),
+              onPressed: () => {},
+            ),
+            const SizedBox(height: 5),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(height: 30),
+              const Text("Don't have an account yet? ", style: TextStyle(color: Colors.white),),
+              InkWell(
+                //onTap: () => goToSignup(context),
+                child:
+                const Text("Signup", style: TextStyle(color: Color(0xFFD2AF43))),
+              )
+            ]),
+            const Spacer()
+          ],
+        ),
+      ),
+    );
+  }}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mala_atlantyda/auth/loginScreen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Inicjalizacja Firebase
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,52 +19,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
